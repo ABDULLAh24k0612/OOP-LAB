322518f35b069e62cdfb1761e68b0c52b21d4f07
ABDULLAh24k0612 <fahadkamran2103@gmail.com>
2025-02-16 10:13:34 -0800

    Update task 01.cpp

--- LAB 04/task 01.cpp
+++ LAB 04/task 01.cpp
@@ -1,1 +1,56 @@
+#include<iostream>
+#include<string>
 
+using namespace std;
+
+
+
+class car{
+	private :
+	string brand;
+	string model;
+	float price;
+	bool availability;
+	
+	public :
+	
+	car(){
+		
+		brand ="unknown";
+		model = "generic";
+		price = 0;
+		availability = 1;
+	}
+	
+	
+	void setcar(string b, string m, float rent, bool a){
+		
+		brand =b;
+		model = m;
+		price = rent;
+		availability =  a;
+	}
+	
+	void requestRent(){
+		
+		if(!availability){
+			cout << "not avialble, select another one if needed" << endl;
+		}else if(availability){
+			
+			cout << "car available , you have booked it" << endl;
+			
+			
+			availability = 0;
+		}
+	}
+};
+
+int main (){
+	
+	car corolla;
+	
+	corolla.requestRent();
+	
+	
+	return 0;
+}
